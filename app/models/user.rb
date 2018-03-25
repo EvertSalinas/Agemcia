44f87cdce72ffac02b2c0952f66130e4da109a1e
Evert Salinas <evertsalinas88@gmail.com>
2018-03-24 19:59:14 -0600

    A lot

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   after_create :auto_confirm_development_user
+
+   private
+
+   def auto_confirm_development_user
+     self.confirm if Rails.env.development?
+   end
 end

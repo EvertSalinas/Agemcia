6c38d47e20c72d7f9d30e4ca227c936f63551f0b
Evert Salinas <evertsalinas88@gmail.com>
2018-04-19 17:22:35 -0500

    Add columns to quotations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403000549) do
+ActiveRecord::Schema.define(version: 20180419220936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20180403000549) do
     t.datetime "updated_at", null: false
     t.datetime "shipment_time"
     t.datetime "pickup_time"
+    t.date "pickup_date"
+    t.date "elaboration_date"
   end
 
   create_table "users", force: :cascade do |t|

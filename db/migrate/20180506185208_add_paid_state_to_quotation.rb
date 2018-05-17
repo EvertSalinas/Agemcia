41c3b81f3f84406ca0ae1de74e15b5689c109a74
Evert Salinas <evertsalinas88@gmail.com>
2018-05-17 15:17:09 -0500

    Paid migration

--- db/migrate/20180506185208_add_paid_state_to_quotation.rb
+++ db/migrate/20180506185208_add_paid_state_to_quotation.rb
@@ -1,4 +1,4 @@
-class AddPaidStatusToQuotations < ActiveRecord::Migration[5.1]
+class AddPaidStateToQuotation < ActiveRecord::Migration[5.1]
   def change
     add_column :quotations, :paid, :boolean
   end
